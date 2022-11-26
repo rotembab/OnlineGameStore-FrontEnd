a8a9a545f42720e62011a34d27e729e862366a15
rotembab <56720556+rotembab@users.noreply.github.com>
2022-11-27 01:26:14 +0200

    init

--- src/Components/CategoryItem.js
+++ src/Components/CategoryItem.js
@@ -3,7 +3,7 @@ export default function CategoryItem(props) {
   return (
     <div className='w-56 h-96 p-4 text-center hover:drop-shadow-xl'>
         <img className="m-auto w-48 h-64   transition ease-in-out delay-100 hover:scale-110" src={props.item.src}/>
-        <div className='py-4'>
+        <div className='py-4 '> 
             <div className='text-white bg-secondary h-32 rounded '>
                 <div className='p-2 h-16'>
                     {props.item.name}
